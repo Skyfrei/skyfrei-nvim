b409666bd9da97c045ef81d8f1786be116513856
Skyfrei <tarkaklavio@gmail.com>
2025-02-21 19:58:08 +0100

    smaller

--- lua/sky/plugin/lsp.lua
+++ lua/sky/plugin/lsp.lua
@@ -6,9 +6,7 @@ return {
     build = ":MasonUpdate",
     opts = {
       ensure_installed = {
-        "clangd",             -- C/C++ 
         "clang-format",       -- C/C++ formatter
-        "codelldb",           -- Debugger
         "rust-analyzer",      -- Rust
         "black",             -- Python formatter
         "stylua",            -- Lua formatter
@@ -20,11 +18,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
-        "clangd",        -- C/C++
         "rust_analyzer", -- Rust
         "lua_ls",       -- Lua
         "pylsp",        -- Python
-        "gopls"
       },
       automatic_installation = true,
     },
@@ -55,27 +51,6 @@ return {
       lspconfig.pylsp.setup({
         capabilities = capabilities,
       })
-
-      -- C/C++
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-        },
-      })
-    lspconfig.gopls.setup({
-        capabilities = capabilities,
-        virtual_text = false,
-        cmd = {
-            "gopls"
-        }
-    })
-
       -- Rust
       lspconfig.rust_analyzer.setup({
         capabilities = capabilities,
@@ -91,53 +66,6 @@ return {
       "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-      })
-    end,
+    }
   },
 }
