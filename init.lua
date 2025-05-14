a449a00bf224362e5700acab04342b6dc2f615db
Skyfrei <tarkaklavio@gmail.com>
2025-05-14 13:44:33 +0200

    change lua exec to visual

--- init.lua
+++ init.lua
@@ -37,5 +37,5 @@ vim.o.pumheight = 2
 
 vim.keymap.set("n", "<C-a>", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-x>", "<Nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>x", ":lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Run lua in visual" })
 
